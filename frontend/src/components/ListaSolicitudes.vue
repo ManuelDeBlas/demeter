c6bc26498536e5458badd7101a3e4c50a439452e
ManuelDeBlas <manu19961996@gmail.com>
2025-04-09 10:48:58 +0200

    Se crea el componente ListadoSolicitudes.

---
+++ frontend/src/components/ListaSolicitudes.vue
@@ -0,0 +1,43 @@
+<script>
+  import { mapState, mapActions } from "pinia";
+  import { useSolicitudesStore } from "@/stores/solicitudes";
+
+  export default {
+    computed: {
+      ...mapState(useSolicitudesStore, ["solicitudes"]),
+    },
+
+    methods: {
+      ...mapActions(useSolicitudesStore, [
+        "anadirSolicitud",
+        "eliminarSolicitud",
+        "modificarEstadoSolicitud",
+      ]),
+      abrirFormularioSolicitud() {
+        // TODO enviar al router que abra el componente FormularioSolicitud
+      }
+    },
+  };
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="titulo p-4">LISTA SOLICITUDES</h1>
+    <button
+      type="button"
+      class="btn btn-success mb-3"
+      @click="abrirFormularioSolicitud"
+    >
+      Añadir
+    </button>
+    <ul>
+      <div
+        v-for="solicitud in solicitudes"
+        :key="solicitud._links.self.href"
+        class="mb-3"
+      >
+        <div>{{ solicitud }}</div>
+      </div>
+    </ul>
+  </div>
+</template>
--- frontend/src/stores/solicitudes.js
+++ frontend/src/stores/solicitudes.js
@@ -30,31 +30,6 @@ export const useSolicitudesStore = defineStore("solicitudes", {
         this.solicitudes.splice(index, 1);
       }
     },
-    anadirSolicitud(solicitudHref, solicitudHref) {
-      console.log("En el store recibo este solicitudHref a añadir solicitud: ", solicitudHref);
-      console.log("En el store recibo este solicitudHref a añadir: ", solicitudHref);
-      const index = this.solicitudes.findIndex(
-        (Solicitud) => Solicitud._links.self.href === solicitudHref
-      );
-      if (index !== -1) {
-        this.solicitudes[index].solicitudes.push(solicitudHref);
-      }
-    },
-    eliminarSolicitud(solicitudHref, solicitudHref) {
-      console.log("En el store recibo este solicitudHref a eliminar solicitud: ", solicitudHref);
-      console.log("En el store recibo este solicitudHref a eliminar: ", solicitudHref);
-      const index = this.solicitudes.findIndex(
-        (Solicitud) => Solicitud._links.self.href === solicitudHref
-      );
-      if (index !== -1) {
-        const solicitudIndex = this.solicitudes[index].solicitudes.findIndex(
-          (solicitud) => solicitud === solicitudHref
-        );
-        if (solicitudIndex !== -1) {
-          this.solicitudes[index].solicitudes.splice(solicitudIndex, 1);
-        }
-      }
-    },
     modificarEstadoSolicitud(solicitudHref, nuevoEstado) {
       console.log("En el store recibo este solicitudHref a modificar: ", solicitudHref);
       console.log("En el store recibo este nuevoEstado a modificar: ", nuevoEstado);

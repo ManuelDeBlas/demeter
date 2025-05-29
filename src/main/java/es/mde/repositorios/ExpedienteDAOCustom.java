cf846895a0207e346477f17192d6cb7a53d88eda
Manuel de Blas Pino <manu19961996@gmail.com>
2025-05-29 20:49:09 +0200

    Se crea el método para saber el coste de un expediente.

--- src/main/java/es/mde/repositorios/ExpedienteDAOCustom.java
+++ src/main/java/es/mde/repositorios/ExpedienteDAOCustom.java
@@ -4,39 +4,16 @@ import es.mde.entidades.ExpedienteConId;
 import es.mde.entidades.SolicitudConId;
 
 /**
- * Repositorio JPA personalizado para gestionar los expedientes.
- * Proporciona métodos CRUD para la entidad {@link ExpedienteConId}.
+ * Repositorio JPA personalizado para gestionar los expedientes. Proporciona
+ * métodos CRUD para la entidad {@link ExpedienteConId}.
  * 
- * Este repositorio está expuesto como un recurso REST con el path "expedientes".
+ * Este repositorio está expuesto como un recurso REST con el path
+ * "expedientes".
  * 
  * @author Manuel de Blas Pino
  * @version 1.0
  */
 public interface ExpedienteDAOCustom {
 
-//  void asignarSolicitudAExpediente(ExpedienteConId expediente, SolicitudConId solicitud);
-  
-//  void eliminarSolicitudDeExpediente(ExpedienteConId expediente, SolicitudConId solicitud);
-
-//  /**
-//   * Asigna una solicitud a un expediente.
-//   * 
-//   * Este método agrega la solicitud al expediente, guarda los cambios en la base de datos
-//   * y registra la operación en los logs.
-//   * 
-//   * @param expediente El expediente al que se asignará la solicitud.
-//   * @param solicitud La solicitud que se asignará al expediente.
-//   */
-//  void asignarSolicitudAExpediente(Long expedienteId, Long solicitudId);
-//
-//  /**
-//   * Elimina una solicitud de un expediente.
-//   * 
-//   * Este método elimina la solicitud del expediente, guarda los cambios en la base de datos
-//   * y registra la operación en los logs.
-//   * 
-//   * @param expediente El expediente del que se eliminará la solicitud.
-//   * @param solicitud La solicitud que se eliminará del expediente.
-//   */
-//  void eliminarSolicitudDeExpediente(Long expedienteId, Long solicitudId);
+  int getCosteCentimosExpedienteByNumeroExpediente(String numeroExpediente);
 }

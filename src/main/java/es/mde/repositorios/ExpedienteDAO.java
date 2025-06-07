9dee3836fda4d28e23b25392dea7abc54f5c50b5
Manuel de Blas Pino <manu19961996@gmail.com>
2025-06-07 13:52:21 +0200

    se expone correctamente el metodo personalizado.

--- src/main/java/es/mde/repositorios/ExpedienteDAO.java
+++ src/main/java/es/mde/repositorios/ExpedienteDAO.java
@@ -1,5 +1,7 @@
 package es.mde.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import es.mde.entidades.ExpedienteConId;
@@ -17,6 +19,6 @@ import es.mde.entidades.ExpedienteConId;
 @RepositoryRestResource(path = "expedientes", itemResourceRel = "expediente", collectionResourceRel = "expedientes")
 public interface ExpedienteDAO extends JpaRepository<ExpedienteConId, Long>, ExpedienteDAOCustom {
 
-  ExpedienteConId getByNumeroExpediente(String numeroExpediente);
+  Optional<ExpedienteConId> findByNumeroExpediente(String numeroExpediente);
 
 }
